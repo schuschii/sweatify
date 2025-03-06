0e92d278b1cd10d0ddfa88260374970d5f776edd
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2025-03-06 20:57:14 +0100

    Fixed merge error and created 3 basic apis

---
+++ sweatify/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExerciseController;
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/exercises', [ExerciseController::class, 'index']);
+Route::get('/exercises/{id}', [ExerciseController::class, 'show']);
+Route::get('/exercises/body-part/{bodyPart}', [ExerciseController::class, 'filterByBodyPart']);
